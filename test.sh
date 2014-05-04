f2026bb0816e8b8d09a775d8499f7e0f9da71e2c
Elliot Wright <wright.elliot@gmail.com>
2014-05-04 23:11:28 +0100

    Updated test.sh.

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-dredd apiary.apib http://192.168.50.30 -l error -h "X-API-App-Token: sufhsdufhsfhof924ir2irh9y34523iu58945io43io8435uo345u345o34j5o9" -h "X-API-User-Token: 42b87b239cc87999937e7c30a350e7917f53b9d22abaf0dbce250d0ebd678bef"
+dredd apiary.apib http://192.168.50.30 --no-color -h "X-API-App-Token: sufhsdufhsfhof924ir2irh9y34523iu58945io43io8435uo345u345o34j5o9" -h "X-API-User-Token: 42b87b239cc87999937e7c30a350e7917f53b9d22abaf0dbce250d0ebd678bef" | grep -E "^(info|pass|fail|completed)"
